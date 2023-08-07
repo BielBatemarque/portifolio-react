0e69aaa173ca3d4771d0cba2a8e43ec644e541b8
GabrielBatemarque <gabriel.oliveira@sci.com.br>
2023-08-07 20:59:33 -0300

    header funcional

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,9 +1,15 @@
-import { Header } from "./styles";
+import { Header, HeaderReduzido, StyledLink } from "./styles";
 
 export const Menu = () => {
     return(
         <Header>
-            opa
+            <HeaderReduzido>
+                <StyledLink>Sobre</StyledLink>
+                <StyledLink>React-router-dom</StyledLink>
+                <StyledLink>React Hooks</StyledLink>
+                <StyledLink>Redux</StyledLink>
+                <StyledLink>Next.Js</StyledLink>
+            </HeaderReduzido>
         </Header>
     );
 }
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -5,8 +5,40 @@ export const Header = styled.header`
     width: 100%;
     color: #fff;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: center;
     text-decoration: none;
     border-bottom: solid 0.5px white;
     line-height: 3.4rem;
+`;
+
+
+export const HeaderReduzido = styled.div`
+    width:85%;
+    display:flex;
+    justify-content: space-evenly;
+`;
+
+export const StyledLink = styled.a`
+    cursor:pointer;
+    color: #FFF;
+    text-decoration: none;
+    position: relative;
+
+    &:after {
+        content: "";
+        position: absolute;
+        left: 50%;
+        bottom: 0;
+        transform: translateX(-50%);
+        width: 0;
+        height: 2px;
+        background-color: #FFF;
+        transition: width 0.3s ease;
+    }
+
+    &:hover {
+        &:after {
+            width: 100%;
+        }
+    }
 `;
