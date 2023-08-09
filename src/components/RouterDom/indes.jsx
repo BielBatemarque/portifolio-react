51f2872704580d34dcbee647637169e21862b514
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-09 18:48:35 -0300

    save repo

--- src/components/RouterDom/indes.jsx
+++ src/components/RouterDom/indes.jsx
@@ -1,10 +1,15 @@
 import { Container } from "../Container";
 import { Title } from "../Title";
+import { FlexContainer } from "./styles";
 
 export const RouterDom = () => {
     return(
         <Container>
             <Title>React-Router-Dom</Title>
+            <FlexContainer>
+                <div></div>
+                <div></div>
+            </FlexContainer>
         </Container>
     );
 }
---
+++ src/components/RouterDom/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const FlexContainer = styled.div`
+    display: flex;
+    background-color: red;
+    justify-content: space-evenly;
+`;
